4eb972587dcbe9d5aaf86dfdafc42c077438fd9e
Anthony Mattox <anthony@friendsoftheweb.com>
2020-03-26 17:35:06 -0400

    Initial Commit

---
+++ Cube Draft Simulator/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Cube Draft Simulator
+//
+//  Created by Anthony Mattox on 3/26/20.
+//  Copyright © 2020 Anthony Mattox. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
